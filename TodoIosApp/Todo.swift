cadc61cc6e6aeceb3a4af48fd29937a001fc35ac
Mohammad Sami <samimd.77@gmail.com>
2018-06-27 00:13:28 -0700

    Added meal model

---
+++ TodoIosApp/Todo.swift
@@ -0,0 +1,41 @@
+//
+//  Todo.swift
+//  TodoIosApp
+//
+//  Created by Mohammad Sami on 6/26/18.
+//  Copyright © 2018 Mohammad Sami. All rights reserved.
+//
+
+import Foundation
+
+class Todo {
+    
+    // MARK: Properties
+    private var _createdAt: Double;
+    private var _title: String;
+    private var _description: String;
+    
+    //MARK: Getters
+    var createdAt: Int64 {
+        get {return _createdAt}
+    }
+    
+    var title: String {
+        get {return _title}
+    }
+    
+    var description: String {
+        get {return _description}
+    }
+    
+    // MARK: Initializers
+    init(title: String, description: String) {
+        self._title = title
+        self._description = description
+        
+        self._createdAt = Date().timeIntervalSince1970
+        
+        print(self._createdAt)
+    }
+    
+}
